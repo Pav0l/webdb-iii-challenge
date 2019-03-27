e40beac8440db310ba4edb6f348a6a63b83d2630
Pav0l <pavol.karas@gmail.com>
2019-03-27 16:27:01 +0100

    Create routes and API endpoints for students DB. Stretch goal finished

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cohortRoutes = require('./routes/cohortsRoutes');
+const studentRoutes = require('./routes/studentsRoutes');
 
 const app = express();
 
@@ -9,6 +10,7 @@ const cohortsUrl = '/api/cohorts';
 const studentsUrl = '/api/students';
 
 app.use(cohortsUrl, cohortRoutes);
+app.use(studentsUrl, studentRoutes);
 
 const PORT = process.env.PORT || 1234
 app.listen(PORT, () => console.log(`Express app listening on http://127.0.0.1:${PORT}`));
